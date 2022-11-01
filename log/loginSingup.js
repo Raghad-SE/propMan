6910127b43d18b63ba1eba18ff7b35995590d8f0
Shahad Hamza <Shahad_18n@hotmail.com>
2022-11-02 01:33:20 +0300

    chang

--- log/loginSingup.js
+++ log/loginSingup.js
@@ -1,31 +1,31 @@
-
 //function to validate the Email address
-function ValidateEmail() {
+/*function ValidateEmail(input) {
   //the pattren that the Email address should take 
   var validRegex = /^[a-zA-Z0-9.!#$%&'+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)$/;
-  var input = document.getElementById("email").value;
+  //var input = document.getElementById("email").value;
   // var input;
-  // document.getElementById("email").innerHTML = input;
-
-
+  //document.getElementById("email").innerHTML = input;
+ //  var valid = true;
   //if it is match return true
   if (input.match(validRegex)) {
-    return true;
+  //  valid = true;
   //if not match return the massege and false 
   } else {
     alert("Invalid Email address!");
-    //document.form1.email.focus();
-    input.preventDefault();
-  }   
+    document.form1.email.focus();
+    //input.preventDefault(); 
+   // valid = false;
+  }  
+ // return valid; 
 }
 
 
 //function to validate the phone number
-function ValidatePhone() {
+function ValidatePhone(phone) {
   //the pattren that the phone number should take		 
   var phoneno = /^\d{10}$/;
   
-  var phone = document.getElementById("phoneNumber").value;
+ // var phone = document.getElementById("phoneNumber").value;
   //if its match return true
   if (phone.match(phoneno))
   {
@@ -39,21 +39,31 @@ function ValidatePhone() {
   }
 }
 
-function CheckPassword() { 
+function CheckPassword(pass) { 
   var decimal=  /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,15}$/;
 
-  var pass = document.getElementById("password").value;
+  //var pass = document.getElementById("password").value;
+  //var valid = true;
 
   if(pass.match(decimal)) 
   { 
     matchingPassword();
+    //if (matchingPassword() == true){
+    //  valid = true;
+    //}else{
+    //  valid = false;
+   // }
   }
   else
   { 
+    password.preventDefault();
+    document.form1.password.focus();
     document.getElementById('message').style.color = 'red';
     document.getElementById('message').innerHTML = 'The password should be [8 to 15 characters which contain at least one lowercase letter, one uppercase letter, one numeric digit, and one special character]';
-    pass.preventDefault();
+    e.preventDefault();
+   // valid = false;
   }
+ // return valid;
 } 
 
 function matchingPassword() {
@@ -61,24 +71,68 @@ function matchingPassword() {
     document.getElementById('confirmPassword').value) {
     document.getElementById('message').style.color = 'green';
     document.getElementById('message').innerHTML = 'matching';
+    return true;
   } else {
     document.getElementById('message').style.color = 'red';
     document.getElementById('message').innerHTML = 'not matching';
     document.getElementById('password').value.preventDefault();
+    return false;
   }
 }
 
 
-function start(){  
+/*function start(){  
   //for singup 
   var singbutton = document.getElementById( "singbutton" );
-  singbutton.addEventListener( "click", ValidatePhone, false );
-  singbutton.addEventListener( "click", ValidateEmail, false );
-  singbutton.addEventListener( "click", CheckPassword, false );
-  //for login
+  singbutton.addEventListener( "click", ValidatePhone );
+  singbutton.addEventListener( "click", ValidateEmail);
+  document.getElementById( "singbutton" ).addEventListener( "click", CheckPassword);
+  //for login  
   var logbutton = document.getElementById( "logbutton" );
   logbutton.addEventListener( "click", ValidateEmail, false );
-
 } // end function start
-  
-window.addEventListener( "load", start, false );
+window.addEventListener( "load", start);
+*/
+const phoneNumber = document.getElementById("phoneNumber");
+const password = document.getElementById("password");
+const phonenumberError = document.getElementById("phonenumber-error");
+const passwordError = document.getElementById("password-error");
+const submitBtn = document.querySelector('#singbutton');
+
+phoneNumber.addEventListener("keyup", () => {
+  if (phoneNumber.value == "") {
+    phonenumberError.classList.add('phonenumber-error');
+    phonenumberError.style.display = "block";
+  } else {
+    phonenumberError.style.display = "none";
+  }
+});
+
+password.addEventListener("keyup", () => {
+  if (password.value == "") {
+    passwordError.style.display = "block";
+  } else {
+    passwordError.style.display = "none";
+  }
+});
+
+submitBtn.addEventListener("click", () => {
+  if (phoneNumber.value == "") {
+    phonenumberError.classList.add('phonenumber-error');
+   // phonenumberError.style.display = "block";
+  }
+  if (password.value == "") {
+    passwordError.style.display = "block";
+  }
+  else{
+      phonenumberError.style.display = 'none';
+      passwordError.style.display = "none";
+      let cred = {
+          password: `${password.value}`,
+          password: `${password.value}`
+      }
+
+      console.log(cred);
+
+  }
+});
